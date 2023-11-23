d1392cb69f0b4eed356b1cf6925f0d6c9db6ed5d
RuiOkazaki <r_okazaki@yumemi.co.jp>
2023-11-23 19:30:28 +0900

    feat: add button to play footsteps and stop all sounds

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -1,19 +1,36 @@
 'use client';
 
-import { Title } from '@mantine/core';
+import { Button, Title } from '@mantine/core';
 import { NextPage } from 'next';
 
 const page: NextPage = () => {
-  // public/sounds/footsteps.mp3を再生する関数
-  const playFootsteps = () => {
-    const audio = new Audio('/sounds/footsteps.mp3');
-    audio.play();
+  let intervalId: NodeJS.Timeout;
+
+  // bpmを引数にして、実際の間隔を計算する関数
+  const calcInterval = (bpm: number): number => {
+    const interval = (60 / bpm) * 1000;
+    return interval;
+  };
+
+  // 特定の間隔で足音を再生する関数
+  const playFootsteps = (bpm: number) => {
+    const interval = calcInterval(bpm);
+    intervalId = setInterval(() => {
+      const footsteps = new Audio('/sounds/footsteps.mp3');
+      footsteps.play();
+    }, interval);
+  };
+
+  // 全ての音声を停止する関数
+  const stopAllSounds = () => {
+    clearInterval(intervalId);
   };
 
   return (
     <>
       <Title>phasmo-tracks</Title>
-      <button onClick={playFootsteps}>足音を再生</button>
+      <Button onClick={() => playFootsteps(115)}>足音を再生</Button>
+      <Button onClick={stopAllSounds}>全ての音声を停止</Button>
     </>
   );
 };
