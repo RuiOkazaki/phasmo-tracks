f345925f6099752f6f442a42bfcdec3c7b199bdd
RuiOkazaki <r_okazaki@yumemi.co.jp>
2023-11-23 18:48:52 +0900

    feat: add footsteps sound and play function

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -1,8 +1,21 @@
+'use client';
+
 import { Title } from '@mantine/core';
 import { NextPage } from 'next';
 
 const page: NextPage = () => {
-  return <Title>phasmo-tracks</Title>;
+  // public/sounds/footsteps.mp3を再生する関数
+  const playFootsteps = () => {
+    const audio = new Audio('/sounds/footsteps.mp3');
+    audio.play();
+  };
+
+  return (
+    <>
+      <Title>phasmo-tracks</Title>
+      <button onClick={playFootsteps}>足音を再生</button>
+    </>
+  );
 };
 
 export default page;
